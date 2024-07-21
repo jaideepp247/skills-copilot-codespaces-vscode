2f63a54912a82c0e28a3b1178651261ad46ba0bd
Jai Deep <108596141+jaideepp247@users.noreply.github.com>
2024-07-21 05:56:47 +0000

    2nd Commit Copilot

---
+++ member.js
@@ -0,0 +1,35 @@
+function skillsMember() {
+    var member = document.getElementById("member");
+    var memberSkill = document.getElementById("memberSkill");
+    var memberSkill2 = document.getElementById("memberSkill2");
+    var memberSkill3 = document.getElementById("memberSkill3");
+    var memberSkill4 = document.getElementById("memberSkill4");
+    var memberSkill5 = document.getElementById("memberSkill5");
+    var memberSkill6 = document.getElementById("memberSkill6");
+    var memberSkill7 = document.getElementById("memberSkill7");
+    var memberSkill8 = document.getElementById("memberSkill8");
+    var memberSkill9 = document.getElementById("memberSkill9");
+    var memberSkill10 = document.getElementById("memberSkill10");
+    var memberSkill11 = document.getElementById("memberSkill11");
+    var memberSkill12 = document.getElementById("memberSkill12");
+    var memberSkill13 = document.getElementById("memberSkill13");
+    var memberSkill14 = document.getElementById("memberSkill14");
+    var memberSkill15 = document.getElementById("memberSkill15");
+
+    memberSkill.style.display = "block";
+    memberSkill2.style.display = "block";
+    memberSkill3.style.display = "block";
+    memberSkill4.style.display = "block";
+    memberSkill5.style.display = "block";
+    memberSkill6.style.display = "block";
+    memberSkill7.style.display = "block";
+    memberSkill8.style.display = "block";
+    memberSkill9.style.display = "block";
+    memberSkill10.style.display = "block";
+    memberSkill11.style.display = "block";
+    memberSkill12.style.display = "block";
+    memberSkill13.style.display = "block";
+    memberSkill14.style.display = "block";
+    memberSkill15.style.display = "block";
+    member.style.display = "none";
+}
